b6aa81d0c97539746c13f6802720488c7c5256a3
Deepali-Kumari9 <deepali.24095@gmail.com>
2026-06-06 18:41:06 +0530

    Create NumberMirror.cpp

---
+++ CodeChef/NumberMirror.cpp
@@ -0,0 +1,21 @@
+/*
+ * Problem          : Number Mirror
+ * Platform         : CodeChef (START01)
+ * Difficulty       : 200
+ *
+ * Approach         : Simply read the input integer
+ *                    and print it back as output.
+ *
+ * Time Complexity  : O(1)
+ * Space Complexity : O(1)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    cout << a;
+    return 0;
+}
